fb1f395eeddd7d4c94a7890cd7d49562ebebcdf5
AwNerd <116765372+AwNerd@users.noreply.github.com>
2022-12-01 11:11:07 -0800

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> answer;
+        sort(nums.begin(), nums.end());
+        if(nums.size() < 3) {
+            return {}; 
+        }
+        if(nums[0] > 0 ) {
+            return {};
+        }
+        unordered_map<int, int> temp;
+        for(int i = 0 ; i < nums.size() ; ++i){     
+            temp[nums[i]] = i;
+        }
+        vector<vector<int>> ans;
+        for(int i = 0 ; i < nums.size() - 2 ; ++i){   
+            if(nums[i] > 0){     
+                break;
+            }
+            for(int j = i + 1 ; j < nums.size() - 1 ; ++j) {    
+                int req = -1*(nums[i] + nums[j]); 
+                if(temp.count(req) && temp.find(req) -> second > j) { 
+                    ans.push_back({nums[i] , nums[j] , required});
+                }
+                j = temp.find(nums[j])->second;
+            }
+            i = temp.find(nums[i])->second;    
+        }
+        return ans;  
+    }
+    };
